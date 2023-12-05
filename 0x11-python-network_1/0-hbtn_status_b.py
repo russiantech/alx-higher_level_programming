ecdce738be1515ddf29c0a086bee095ec0899c60
russiantech <chrisjsmez@gmail.com>
2023-12-05 04:57:50 -0800

    0-hbtn_status_b.py

---
+++ 0x11-python-network_1/0-hbtn_status_b.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Fetch https://alx-intranet.hbtn.io/status using urlib package
+
+
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
+        content = r.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
