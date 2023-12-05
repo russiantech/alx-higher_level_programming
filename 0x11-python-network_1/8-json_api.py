75fd6d4cd023ee82a55077259192512fe26c1b00
russiantech <chrisjsmez@gmail.com>
2023-12-05 04:17:43 -0800

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """
-Takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
-with the letter as a parameter. Displays the id and name if the JSON response is
-properly formatted and not empty. Otherwise, displays appropriate messages.
+Takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+Displays the id and name if the JSON response is
+properly formatted and not empty.
+Otherwise, displays appropriate messages.
 """
 
 import requests
